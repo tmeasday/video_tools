8bf1eef0bc629ef7b5a739e9577b6ead99e2ac90
Tom Coleman <tom@thesnail.org>
2010-08-22 12:25:41 +1000

    Initial commit of files

---
+++ convert.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+DIR         = ARGV[0].sub /\/$/, ''
+INPUT_DIR   = "#{DIR}"
+STAGING_DIR = "#{DIR}/../Staging"
+VIDEO_DIR   = "#{DIR}/../Video"
+MUSIC_DIR   = "#{DIR}/../Music"
+OTHER_DIR   = "#{DIR}/../Other"
+
+
+MUX_MOVIE = 'Resources/muxmovie'
+NOTHING   = 'Resources/nothing.mov'
+
+require 'convert_utils'
+
+puts
+puts "Conversion at " + Time.now.to_s
+
+puts "Staging files.."
+# files = Dir.glob("#{INPUT_DIR}/**/*.avi")
+# scan out directories
+files = ARGV[1..-1].map do |f|
+  if File.directory?(f)
+    Dir.glob "#{f}/**/*.*"
+  else
+    f
+  end
+end.flatten
+
+
+files.map! { |f| stage_file(f) }
+clean_dir(INPUT_DIR)
+
+puts "Converting files.."
+files.map do |f|
+  if f =~ /.avi$/
+    convert_file(f)
+  elsif f =~ /.(m4v|mp4|mov)/
+    move_file(f, STAGING_DIR, VIDEO_DIR)
+  elsif f =~ /.(mp3|m4a|aac)/
+    move_file(f, STAGING_DIR, MUSIC_DIR)
+  else
+    move_file(f, STAGING_DIR, OTHER_DIR)
+  end
+end
+clean_dir(STAGING_DIR)
+
+puts "Done at " + Time.now.to_s
---
+++ convert_utils.rb
@@ -0,0 +1,50 @@
+require 'ftools'
+require 'fileutils'
+
+# make sure the dirs exist to move file name from inside from to the equivalent
+# position inside to
+#   e.g. from/a/b/c.txt -> to/a/b/c.txt
+def clone_path(filename, from, to, new_ext = nil)
+  # strip the relevant part of the path from the filename
+  # will throw an exception if the path doesn't start with from...
+  old_path, basename = File.split(filename)
+  path = old_path.match("#{from}(.*)")[1]  
+  new_path = File.join(to, path)
+  
+  # ensure that new_path exists
+  File.makedirs new_path
+  
+  # return the 'new' file name
+  File.join(new_path, new_ext ? basename.sub(/\.[^\.]*$/, new_ext) : basename )
+end
+
+
+def move_file(in_file, from_dir, to_dir)
+  new_file = clone_path(in_file, from_dir, to_dir)
+  puts "Moving #{in_file} to #{new_file}"
+  File.move(in_file, new_file, false)
+  
+  new_file
+end
+
+def stage_file(in_file)
+  move_file(in_file, INPUT_DIR, STAGING_DIR)
+end
+
+def convert_file(in_file)
+  out_file = clone_path(in_file, STAGING_DIR, VIDEO_DIR, '.mov')
+  puts "Converting #{in_file} => #{out_file}"
+  # muxmovie doesn't give us any nice error status, so lets hope it doesn't print nothing
+  if %x[#{MUX_MOVIE} -self-contained #{NOTHING} "#{in_file}" -o "#{out_file}" 2>&1] == ''
+    FileUtils.rm(in_file)
+  else
+    puts "Error converting #{in_file}.. skipping"
+  end
+  
+  out_file
+end
+
+# delete all the empty directories out of the dir
+def clean_dir(dir)
+  Dir.glob(File.join(dir, '*')).each {|d| clean_dir(d); Dir.rmdir(d) rescue nil}
+end
